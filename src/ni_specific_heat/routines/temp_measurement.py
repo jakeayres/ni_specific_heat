81f13972bf7c60fa68aa627c6676f61a7c416865
CharlieW5 <147152654+CharlieW5@users.noreply.github.com>
2026-02-10 10:53:24 +0000

    Refactor temperature measurement to use asyncio
    Refactor temperature measurement methods to use asyncio for better performance and responsiveness. Updated method names and logic for queuing and processing temperature data.

--- src/ni_specific_heat/routines/temp_measurement.py
+++ src/ni_specific_heat/routines/temp_measurement.py
@@ -10,7 +10,7 @@ from ..ui.indicators import DecimalIndicator, ScientificDecimalIndicator, Intege
 from ..ui.inputs import DecimalInput, IntegerInput, StringInput
 from ..ui.plot import Plot
 
-def temp_measure(self):
+def setup_temp_measure(self):
 
 	temp_window = gui.add_window(label='Temperature', pos=(0, 0), width=300)
 	self.live_temperature_plot = Plot.add_to_parent(temp_window, height=200, width=-1)
@@ -38,18 +38,33 @@ def temp_measure(self):
 		)
 	)
 
-def queue_temp_measure(self,data):
-	self.temp_measurement_running = 1
+def queue_temp_measure(self):
+	self.temp_measure_task = asyncio.create_task(self.measurement_loop(self.data))
+	
+async def measurement_loop(self,data):
+	temp_points = [[], [], []]
 	start = time.time()
-	temp_points = [[],[],[]]
-	self.tasks.append(partial(
-		while self.temp_measurement_running == 1:
-			temp_points[0].append(time.time() - start)
-			temp_points[1].append(data['temperature']),
-			temp_points[2].append(data['rate']),
-		#save data
-		temp_points.to_csv(f'C://Data/{file_stem}_temp_measurement', index=False, header=['time', 'temperature']),
-	))
+	while True:
+		await asyncio.sleep(1)
+		try:
+			self.temp_update(data,temp_points)
+		except Exception as e:
+			logger.error(f"Error in temp measurement: {e}")
+	#save data
+	temp_points_df = pd.DataFrame({
+		'time': temp_points[0],
+		'temperature': temp_points[1],
+		'rate': temp_points[2]
+	})
+	temp_points_df.to_csv(f'C://Data/{file_stem}_temp_measurement.csv', index=False, header=['time', 'temperature', 'rate'])
+
+def temp_update(self,data,temp_points):
+	temp_points[0].append(time.time() - start)
+	temp_points[1].append(data['temperature'])
+	temp_points[2].append(data['rate'])
+
 
 def stop_temp_measure(self):
-	self.temp_measurement_running = 0
+	task = getattr(self, "_measurement_task", None)
+	if task and not task.done():
+		task.cancel()
